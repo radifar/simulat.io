cef0f1368a672a3189de5dacfbf51133a160cadc
radifar <m.radifar05@gmail.com>
2020-01-19 23:03:15 +0700

    All docking components for PoC finished

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -35,7 +35,21 @@ import HistoryPlugin from "rete-history-plugin";
 import * as ConnectionMasteryPlugin from "rete-connection-mastery-plugin";
 import { saveAs } from "file-saver";
 
+// Socket definition
+
 var numSocket = new Rete.Socket("Number value");
+var molSocket = new Rete.Socket("Molecule value");
+var bindDefSocket = new Rete.Socket("Binding site definition");
+var resultSocket = new Rete.Socket("Save result to DB")
+
+/* 
+  Vue Control
+  1. VueNumControl
+  2. VueTextControl
+  3. VueOptControl
+  4. VueCheckboxControl
+  5. VueInputFileControl
+*/
 
 var VueNumControl = {
   props: ["readonly", "emitter", "ikey", "getData", "putData"],
@@ -51,6 +65,33 @@ var VueNumControl = {
       console.log(e)
       console.log(this.ikey)
       this.value = +e.target.value;
+      this.update();
+    },
+    update() {
+      if (this.ikey) this.putData(this.ikey, this.value);
+      this.emitter.trigger("process");
+    }
+  },
+  mounted() {
+    this.value = this.getData(this.ikey);
+  }
+};
+
+var VueTextControl = {
+  props: ["readonly", "emitter", "ikey", "placeholder", "label", "getData", "putData"],
+  template:
+    '<div>\
+    {{label}}\
+    <input type="text" :placeholder="placeholder" :readonly="readonly" :value="value" @input="change($event)" @dblclick.stop="" @pointermove.stop=""/>\
+    </div>',
+  data() {
+    return {
+      value: 0
+    };
+  },
+  methods: {
+    change(e) {
+      this.value = e.target.value;
       this.update();
     },
     update() {
@@ -91,6 +132,35 @@ var VueOptControl = {
   }
 };
 
+var VueCheckboxControl = {
+  props: ["readonly", "emitter", "ikey", "label", "getData", "putData"],
+  template:
+  '<div>\
+  <table><tr>\
+  <td><div class="control-title">{{label}}:</div></td>\
+  <td><input type="checkbox" :name="label"></td>\
+  </tr></table>\
+  </div>'
+};
+
+var VueInputFileControl = {
+  props: ["readonly", "emitter", "ikey", "label", "getData", "putData"],
+  template:
+  '<div>\
+  <div class="control-title">{{label}}:</div>\
+  <input type="file" name="ligand" accept="text/*">\
+  </div>'
+};
+
+/*
+  Controller class:
+  1. NumControl
+  2. TextControl
+  3. OptControl
+  4. CheckboxControl
+  5. InputFileControl
+*/
+
 class NumControl extends Rete.Control {
   constructor(emitter, key, readonly) {
     super(key);
@@ -102,14 +172,210 @@ class NumControl extends Rete.Control {
   }
 }
 
+class TextControl extends Rete.Control {
+  constructor(emitter, key, placeholder, label, readonly) {
+    super(key);
+    this.component = VueTextControl;
+    this.props = { emitter, ikey: key, placeholder, label, readonly };
+  }
+  setValue(val) {
+    this.vueContext.value = val;
+  }
+}
+
 class OptControl extends Rete.Control {
   constructor(emitter, key, options, readonly) {
     super(key);
     this.component = VueOptControl;
-    this.props = { emitter, ikey: key, options: options, readonly };
+    this.props = { emitter, ikey: key, options, readonly };
   }
   setValue(val) {
     this.vueContext.value = val;
+  }
+}
+
+class CheckboxControl extends Rete.Control {
+  constructor(emitter, key, label, readonly) {
+    super(key);
+    this.component = VueCheckboxControl;
+    this.props = { emitter, ikey: key, label, readonly };
+  }
+  setValue(val) {
+    this.vueContext.value = val;
+  }
+}
+
+class InputFileControl extends Rete.Control {
+  constructor(emitter, key, label, readonly) {
+    super(key);
+    this.component = VueInputFileControl;
+    this.props = { emitter, ikey: key, label, readonly };
+  }
+  setValue(val) {
+    this.vueContext.value = val;
+  }
+}
+
+/* 
+  Component:
+  1. OpenLigandComp
+  2. PDBSplitComp
+  3. PLANTSBindComp
+  4. CombineLigandComp
+  5. OpenbabelConvComp
+  6. PLANTSDockComp
+  7. VINADockComp
+  8. SaveDBComp
+*/
+
+class OpenLigandComp extends Rete.Component {
+  constructor() {
+    super("Ligand Input")
+    this.path = ["Input"];
+  }
+
+  builder(node) {
+    var out1 = new Rete.Output("lig1", "Ligand", molSocket);
+
+    return node.addControl(new InputFileControl(this.editor, "lig1", "Ligand")).addOutput(out1);
+  }
+}
+
+class PDBSplitComp extends Rete.Component {
+  constructor() {
+    super("PDB Split")
+    this.path = ["Input"];
+  }
+
+  builder(node) {
+    var out1 = new Rete.Output("lig1", "Ligand", molSocket);
+    var out2 = new Rete.Output("prot1", "Protein", molSocket);
+
+    return node
+      .addControl(new InputFileControl(this.editor, "PDBFile", "PDB File"))
+      .addControl(new TextControl(this.editor, "ligID", "Ligand ID"))
+      .addOutput(out1)
+      .addOutput(out2);
+  }
+}
+
+class PLANTSBindComp extends Rete.Component {
+  constructor() {
+    super("PLANTS Binding Site");
+    this.path = ["Docking"];
+  }
+
+  builder(node) {
+    let inLigand = new Rete.Input("inLigand", "Input Ligand", molSocket)
+    let outBindDef = new Rete.Output("outBindDef", "Binding site definition", bindDefSocket)
+
+    return node
+      .addInput(inLigand)
+      .addOutput(outBindDef)
+      .addControl(new TextControl(this.editor, "radius", "5", "radius"))
+  }
+}
+
+class CombineLigandComp extends Rete.Component {
+  constructor() {
+    super("Combine Ligand");
+    this.path = ["Cheminformatics"];
+  }
+
+  builder(node) {
+    let inLigand1 = new Rete.Input("inLigand1", "Input Ligand 1", molSocket)
+    let inLigand2 = new Rete.Input("inLigand2", "Input Ligand 2", molSocket)
+    let outLigand = new Rete.Output("outLigand", "Output Ligand", molSocket)
+
+    return node
+      .addInput(inLigand1)
+      .addInput(inLigand2)
+      .addOutput(outLigand)
+  }
+}
+
+class OpenbabelConvComp extends Rete.Component {
+  constructor() {
+    super("Openbabel Conversion");
+    this.path = ["Cheminformatics"];
+    this.inOptions = [
+      {value: "", name: "Input Format"},
+      {value: "mol2", name: "Mol2"},
+      {value: "pdb", name: "PDB"},
+      {value: "pdbqt", name: "PDBQT"},
+      {value: "smiles", name: "SMILES"}
+    ];
+    this.outOptions = [
+      {value: "", name: "Output Format"},
+      {value: "mol2", name: "Mol2"},
+      {value: "pdb", name: "PDB"},
+      {value: "pdbqt", name: "PDBQT"},
+      {value: "smiles", name: "SMILES"}
+    ];
+  }
+
+  builder(node) {
+    let inMol  = new Rete.Input("inMol", "Input Molecule", molSocket);
+    let outMol = new Rete.Output("prot", "Output Molecule", molSocket);
+
+    return node
+      .addInput(inMol)
+      .addOutput(outMol)
+      .addControl(new CheckboxControl(this.editor, "gen3D", "gen3D"))
+      .addControl(new OptControl(this.editor, "inFormat", this.inOptions))
+      .addControl(new OptControl(this.editor, "outFormat", this.outOptions))
+  }
+}
+
+class PLANTSDockComp extends Rete.Component {
+  constructor() {
+    super("Docking with PLANTS");
+    this.path = ["Docking"];
+  }
+
+  builder(node) {
+    let inLigand = new Rete.Input("inLigand", "Input Ligand", molSocket);
+    let inProtein = new Rete.Input("inProtein", "Input Protein", molSocket);
+    let inBindDef = new Rete.Input("inBindDef", "Binding Site Definition", bindDefSocket);
+    let outSaveResult = new Rete.Output("outSaveResult", "Docking Result", resultSocket);
+
+    return node
+      .addInput(inLigand)
+      .addInput(inProtein)
+      .addInput(inBindDef)
+      .addOutput(outSaveResult)
+  }
+}
+
+class VINADockComp extends Rete.Component {
+  constructor() {
+    super("Docking with VINA");
+    this.path = ["Docking"];
+  }
+
+  builder(node) {
+    let inLigand = new Rete.Input("inLigand", "Input Ligand", molSocket);
+    let inProtein = new Rete.Input("inProtein", "Input Protein", molSocket);
+    let inBindDef = new Rete.Input("inBindDef", "Binding Site Definition", bindDefSocket);
+    let outSaveResult = new Rete.Output("outSaveResult", "Docking Result", resultSocket);
+
+    return node
+      .addInput(inLigand)
+      .addInput(inProtein)
+      .addInput(inBindDef)
+      .addOutput(outSaveResult)
+      .addControl(new TextControl(this.editor, "boxSize", "12", "Box Size"))
+  }
+}
+
+class SaveDBComp extends Rete.Component {
+  constructor() {
+    super("Save to Database");
+    this.path = ["Output"];
+  }
+
+  builder(node) {
+    return node.addInput(new Rete.Input("inResult", "Result", resultSocket))
   }
 }
 
@@ -182,12 +448,20 @@ export default {
   async mounted() {
     var container = document.querySelector("#rete");
     var components = [
-      new NumComponent(),
-      new AddComponent(),
-      new ProtLigandExtractComponent()
+      new OpenLigandComp(),
+      new PDBSplitComp(),
+      new OpenbabelConvComp(),
+      new CombineLigandComp(),
+      new PLANTSBindComp(),
+      new PLANTSDockComp(),
+      new VINADockComp(),
+      new SaveDBComp(),
+      // new NumComponent(),
+      // new AddComponent(),
+      // new ProtLigandExtractComponent()
     ];
 
-    var editor = new Rete.NodeEditor("workflow@0.1.0", container);
+    var editor = new Rete.NodeEditor("simulatio@0.1.0", container);
     editor.use(ConnectionPlugin.default);
     editor.use(VueRenderPlugin.default);
     editor.use(ContextMenuPlugin.default, {
@@ -211,16 +485,17 @@ export default {
     editor.use(HistoryPlugin);
     editor.use(ConnectionMasteryPlugin.default);
 
-    // var engine = new Rete.Engine("workflow@0.1.0");
+    // var engine = new Rete.Engine("simulatio@0.1.0");
 
     components.map(c => {
       editor.register(c);
       //  engine.register(c);
     });
 
-    var n1 = await components[0].createNode({ num1: 2 });
-    var n2 = await components[0].createNode({ num1: 4 });
-    var add = await components[1].createNode();
+    /*
+    var n1 = await components[2].createNode({ num1: 2 });
+    var n2 = await components[2].createNode({ num1: 4 });
+    var add = await components[3].createNode();
 
     n1.position = [80, 200];
     n2.position = [80, 400];
@@ -232,21 +507,7 @@ export default {
 
     editor.connect(n1.outputs.get("num1"), add.inputs.get("num1"));
     editor.connect(n2.outputs.get("num1"), add.inputs.get("num2"));
-
-    /*
-    editor.on(
-      "process nodecreated noderemoved connectioncreated connectionremoved",
-      async () => {
-        console.log(
-          "process"
-        );
-          console.log(n1);
-          console.log(n2);
-        //console.log(editor.toJSON());
-        await engine.abort();
-        await engine.process(editor.toJSON());
-      }
-    );*/
+    */
 
     editor.view.resize();
     AreaPlugin.zoomAt(editor);
@@ -259,6 +520,11 @@ export default {
 .btn-sm {
   padding: 5px 5px !important;
   margin: 8px 5px !important;
+  width: 95px !important;
+}
+
+.form-control {
+  font-size: 0.9rem;
 }
 
 #rete {
@@ -268,13 +534,12 @@ export default {
   height: 801px !important;
   border-radius: 10px;
   stroke-width: 23;
-  /* background was downloaded from hwww.subtlepatterns.com  */
   background: url("/img/grid1.png");
 }
 
 .node .control input,
 .node .input-control input {
-  width: 140px;
+  width: 60px;
 }
 
 .context-menu {
@@ -363,7 +628,7 @@ select, input {
 }
 
 .socket {
-  border: 1.5px solid gainsboro !important;
+  border: 2px solid gainsboro !important;
   width: 18px !important;
   height: 18px !important;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
@@ -380,6 +645,11 @@ select, input {
 }
 
 .node .output-title {
+  color: gray !important;
+  margin: 4px !important;
+}
+
+.node .control-title {
   color: gray !important;
   margin: 4px !important;
 }
@@ -404,11 +674,39 @@ span.controlLabel {
 }
 
 input[type="text"] {
+  display: block;
   border-radius: 4px;
   font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif !important;
   -webkit-font-smoothing: antialiased;
-  width: 180px !important;
-  border: solid 1px gainsboro;
+  width: 160px !important;
+  padding: .3em .5em .4em .6em;
+  border: 1px solid #aaa;
+  box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+  border-radius: .4em;
+  -moz-appearance: textfield;
+  background-color: #fff;
+  background-image: linear-gradient(to bottom, #ffffff 0%,#efefef 100%);
+  background-repeat: repeat, repeat;
+  background-position: right .7em top 50%, 0 0;
+  background-size: .65em auto, 100%;
+}
+
+input[type="file"] {
+  display: block;
+  border-radius: 4px;
+  font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif !important;
+  -webkit-font-smoothing: antialiased;
+  width: 200px !important;
+  padding: .3em .5em .4em .6em;
+  border: 1px solid #aaa;
+  box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+  border-radius: .4em;
+  -moz-appearance: textfield;
+  background-color: #fff;
+  background-image: linear-gradient(to bottom, #ffffff 0%,#efefef 100%);
+  background-repeat: repeat, repeat;
+  background-position: right .7em top 50%, 0 0;
+  background-size: .65em auto, 100%;
 }
 
 input[type="number"] {
@@ -441,7 +739,7 @@ select {
   border-radius: 4px;
   font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif !important;
   -webkit-font-smoothing: antialiased;
-  width: 180px !important;
+  width: 130px !important;
   padding: .3em .5em .4em .6em;
   border: 1px solid #aaa;
   box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
@@ -455,6 +753,34 @@ select {
   background-repeat: no-repeat, repeat;
   background-position: right .7em top 50%, 0 0;
   background-size: .65em auto, 100%;
+}
+
+.save-result-to-db {
+  background: darkviolet !important;
+}
+
+.socket-input-save-result-to-db .main-path {
+  stroke: darkviolet !important;
+}
+
+.molecule-value {
+  background:hotpink !important;
+}
+
+.socket-input-molecule-value .main-path {
+  stroke: hotpink !important;
+}
+
+.binding-site-definition {
+  background: #999 !important;
+}
+
+.socket-input-binding-site-definition .main-path {
+  stroke: #999 !important;
+}
+
+.frame-comment, .inline-comment {
+  color:rgba(0, 0, 0, 0.76) !important;
 }
 </style>
 
